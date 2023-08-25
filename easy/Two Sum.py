5c8219324e8f5f60bb2c577579791260915a15eb
gronck2 <astepanov.waves@gmail.com>
2023-08-25 14:10:09 +0400

    Problem 1 `Two Sum`

---
+++ easy/Two Sum.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        hash_nums = {v: i for i, v in enumerate(nums)}
+
+        for i, v in enumerate(nums):
+            s = target - v
+            if s in hash_nums and hash_nums[s] != i:
+                return [i, hash_nums[s]]
+
+
+print(Solution().twoSum([3, 3], 6))
